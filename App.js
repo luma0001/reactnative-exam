d1a62b4a5b476e76e020ee04419e7226f5e977e3
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-12-23 12:36:12 +0100

    Added react-map

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { app } from "./firebase";
+import MapView from "react-native-maps";
+
 
 export default function App() {
+  // alert(JSON.stringify(app, null, 4));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}></MapView>
     </View>
   );
 }
@@ -14,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
